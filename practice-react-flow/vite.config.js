b2619a28714c08cc816655e1df26f683d5fe7353
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2025-05-20 13:14:55 +0600

    add 10 design pattrn code

--- practice-react-flow/vite.config.js
+++
@@ -1,7 +0,0 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-});
